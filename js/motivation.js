025724cfeba59c5e908034cb3d81ab1638302478
Josh <programmmerjosh@gmail.com>
2022-01-02 13:02:47 +0000

    Minor updates
    Changed the language from blog to something less particular/specific.

--- js/motivation.js
+++ js/motivation.js
@@ -56,14 +56,14 @@
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
 
-$(document).ready(function(){
-    $("#testimonial-slider").owlCarousel({
-        items:3,
-        itemsDesktop:[1000,3],
-        itemsDesktopSmall:[979,2],
-        itemsTablet:[768,2],
-        itemsMobile:[650,1],
-        pagination:true,
-        autoPlay:true
-    });
-});
+// $(document).ready(function(){
+//     $("#testimonial-slider").owlCarousel({
+//         items:3,
+//         itemsDesktop:[1000,3],
+//         itemsDesktopSmall:[979,2],
+//         itemsTablet:[768,2],
+//         itemsMobile:[650,1],
+//         pagination:true,
+//         autoPlay:true
+//     });
+// });
