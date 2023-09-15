4ef0083603e6c3f2a4826dbdab7a7613c4d07b0a
Josh <programmmerjosh@gmail.com>
2023-09-15 09:51:58 +0100

    navbar light background effect implemented

--- src/observer.js
+++ src/observer.js
@@ -1,28 +1,41 @@
-const welcomeSection = document.querySelector(".info-section");
+// Here, we are setting up an intersection observer
+// this observer is looking for the .masterhead element,
+// when found, it removes the light background and dark-text of the navbar
+
+// page sections we are going to observe
+const headerSection = document.querySelector(".masthead");
+
+// elements to change
+const navbar = document.querySelector(".navbar");
+const navbarLinks = document.querySelectorAll(".nav-link");
+const navbarBrand = document.querySelector(".navbar-brand");
 
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      entry.target.classList.toggle("bg-light", entry.isIntersecting);
-      welcomeSection.classList.toggle(
-        "hellloooooooooooo",
-        entry.isIntersecting
-      );
+      if (entry.isIntersecting) {
+        navbar.classList.remove("bg-light");
+        navbarBrand.classList.remove("text-dark");
+
+        navbarLinks.forEach((link) => {
+          link.classList.remove("text-dark");
+        });
+      } else {
+        navbar.classList.add("bg-light");
+        navbarBrand.classList.add("text-dark");
+
+        navbarLinks.forEach((link) => {
+          link.classList.add("text-dark");
+        });
+      }
+
       // this line cancels the observer after running once
       // if (entry.isIntersecting) observer.unobserve(entry.target);
     });
-
-    console.log(entries);
   },
   {
-    threshold: 0,
-    rootMargin: "-200px",
+    threshold: 0.1,
   }
 );
 
-const navs = document.querySelectorAll(".contact1-form");
-
-navs.forEach((nav) => {
-  console.log(nav.innerHTML);
-  observer.observe(nav);
-});
+observer.observe(headerSection);
