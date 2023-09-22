ae0f6dfea0152c5780c241b4fc03380f66c840b9
Josh <programmmerjosh@gmail.com>
2023-09-22 19:55:44 +0100

    fix contact button responsiveness

--- src/components/shared/ContactButton.vue
+++ src/components/shared/ContactButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fixed-floating-buttons ffb-one">
     <a className="rectangle-link-box" href="#contact">
-      <div className="row d-block">
+      <div className="row">
         <div className="text-center">
           <i className="far fa-paper-plane rectangle-link-box-img mb-2"></i>
           <p className="text-uppercase">contact</p>
@@ -10,3 +10,16 @@
     </a>
   </div>
 </template>
+
+<style scoped>
+  .rectangle-link-box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  @media (max-width: 400px) {
+    .fa-paper-plane {
+      font-size: 0.8rem;
+    }
+  }
+</style>
