ca783124bba9d6872b04d03345d1b299da4f42c6
programmmerjosh <programmmerjosh@gmail.com>
2026-08-21 16:17:20 +0100

    fix css and bootstrap loading

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import { createHead } from "@vueuse/head";
-import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css";
 import "./assets/motivation.min.css";
 
 import App from "./App.vue";
