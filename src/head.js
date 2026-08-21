e1d45f48272018bcad98352a567b2b3ee592f314
programmmerjosh <programmmerjosh@gmail.com>
2026-08-21 14:06:19 +0100

    add bootstrap back to link array

--- src/head.js
+++ src/head.js
@@ -88,8 +88,11 @@ export default {
       rel: "stylesheet",
     },
     {
-      href: "https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i",
+      href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css",
       rel: "stylesheet",
+      integrity:
+        "sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6",
+      crossorigin: "anonymous",
     },
   ],
   script: [
